f4fa08a63ad8d87fe57475f525d0ee1133e7d19a
DinhDucDat Nguyen <nguyedin@flip2.engr.oregonstate.edu>
2025-04-08 17:24:10 -0700

    from flip

--- project1/run.bash
+++ project1/run.bash
@@ -5,7 +5,7 @@ for num_thread in 1 2 4 6 8
 do
     for num_trial in 100 1000 10000 50000 100000
     do
-            /usr/bin/g++ -DNUMT=$num_thread -DNUMTRIALS=$num_trial main.cpp -o project1 -lm -fopenmp  # Full path to g++-13
+            /usr/bin/g++ -DCSV=true -DNUMT=$num_thread -DNUMTRIALS=$num_trial main.cpp -o project1 -lm -fopenmp  # Full path to g++-13
             ./project1
     done
 done
