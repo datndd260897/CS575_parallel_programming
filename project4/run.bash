c0e5eb54f617373c331826a19a6b5995ac7ce428
Dat Nguyen <datndd260897@gmail.com>
2025-05-11 14:09:34 -0700

    Projcet4

---
+++ project4/run.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+SIZE=1024
+LIMIT=9000000
+echo 'SIZE, NUMT, Mul Pnon-sse, Mul Psse, Mul S, MulOMP Psse, MulOMP S, MulSum Pnon-sse, MulSum Psse, MulSum S, MulSumOMP Psse, MulSumOMP S'
+for t in 2 4 6
+do
+    while [ $SIZE -le $LIMIT ]
+    do
+        g++ main.cpp -DNUMT=$t -DARRAYSIZE=$SIZE -o project4 -lm -fopenmp
+        ./project4
+        SIZE=$((SIZE * 2))
+    done
+    SIZE=1024
+done
