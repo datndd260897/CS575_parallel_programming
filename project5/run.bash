465399ab2231a8f8aba44d1aacc6e04dd788ed3f
DinhDucDat Nguyen <nguyedin@rabbit.engr.oregonstate.edu>
2025-05-20 22:06:02 -0700

    out.txt

--- project5/run.bash
+++ project5/run.bash
@@ -2,7 +2,7 @@ for t in 1024 4096 16384 65536 262144 1048576 2097152
 do
     for b in 8 32 64 128 256
     do
-        nvcc -DNUMTRIALS=$t -DBLOCKSIZE=$b -o proj05 proj05.cu
-        ./proj05
+	/usr/local/apps/cuda/cuda-10.1/bin/nvcc -DNUMTRIALS=$t -DBLOCKSIZE=$b -o proj05 proj05.cu
+	./proj05            
     done
 done
