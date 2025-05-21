1f405e20df3b08e166ab2663546ec1ff7c5b1c3c
Dat Nguyen <datndd260897@gmail.com>
2025-05-20 21:10:34 -0700

    project5

---
+++ project5/run.bash
@@ -0,0 +1,8 @@
+for t in 1024 4096 16384 65536 262144 1048576 2097152
+do
+    for b in 8 32 64 128 256
+    do
+        nvcc -DNUMTRIALS=$t -DBLOCKSIZE=$b -o proj05 proj05.cu
+        ./proj05
+    done
+done
